3e280594269e709647bfc82d25510ff51a835a9c
OussamaHaddi <oussama@gmail.com>
2022-06-20 01:20:24 +0100

    add js to buttons

--- js/main.js
+++ js/main.js
@@ -8,7 +8,11 @@ let navBar = document.querySelector("#nav-bar");
     ul = document.querySelector(".blured");
     body = document.body;
     closeBtn = document.querySelector("#close-icon");
-    cartBox = document.querySelector("#cart-box")
+    cartBox = document.querySelector("#cart-box");
+    nextImg = document.querySelector(".next");
+    previousImg = document.querySelector(".previous");
+    allImg = document.querySelectorAll(".small");
+    mainImg = document.querySelector(".main-img");
 
 // show and hide nav bar
 
@@ -30,6 +34,37 @@ function show() {
     }
 }
 
-cart.addEventListener("click", show)
+cart.addEventListener("click", show);
+
+
+// show next and previous img
+
+allImg.forEach(el => {
+    el.addEventListener("click", function () {
+        // remove ativated class
+        allImg.forEach(img => img.classList.remove("selected-img"));
+        mainImg.src = this.src;
+        this.classList.add("selected-img");
+    })
+});
+
+let myImage = ["./sass/images/image-product-1.jpg", "./sass/images/image-product-2.jpg", "./sass/images/image-product-3.jpg", "./sass/images/image-product-4.jpg"]
+let i = 0;  // current index of image
+
+nextImg.onclick = function () {
+    if (i <= 0) i = myImage.length;
+    i--;
+    return setFunc();
+}
+
+previousImg.onclick = function () {
+    if (i >= myImage.length - 1) i = -1;
+    i++;
+    return setFunc();
+}
+
+function setFunc() {
+    return mainImg.src = myImage[i];
+}
 
 
